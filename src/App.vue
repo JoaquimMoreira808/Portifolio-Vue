ebf2ca270a3b9287e4822609f81bfc1955728c97
JoaquiMoreira808 <joaquimfernando808@gmail.com>
2025-05-29 19:15:53 +0100

    Primeiro Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen bg-blue-300">
+    <div class="container mx-auto px-4 py-6">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="p-10">
+      <p class="text-center font-bold text-4xl">
+        Portifolio
+      </p>
+    </h1>
+    <div class="flex items-center justify-center">
+      <img class="size-48 object-fill shadow-xl object-center rounded-full" src="../imgs/profilepic.jpeg" alt="">
+    </div>
+    <div class="p-10">
+      <p class="truncate text-center text-3x1 font-semibold">Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse consectetur accumsan lacus, vel congue elit finibus sed. </p>
+    </div>
+  </div>
+</template>
+
+
